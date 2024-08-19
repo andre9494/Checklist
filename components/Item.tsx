139d2cccdeddc0a380323196460a4f7666537ea7
Andre Pereira <andre.9494@gmail.com>
2024-08-19 18:32:37 +0100

    item editable

--- components/Item.tsx
+++ components/Item.tsx
@@ -4,14 +4,30 @@ import Checkbox from "expo-checkbox";
 import Container from "../layouts/Container";
 import { useState } from "react";
 import { COLORS } from "../styles";
+import Button from "./Button";
+import IconButton from "./IconButton";
 
-const Item = (props: { text: string; edit?: boolean; onBlur: () => void }) => {
-  const { text, edit, onBlur } = props;
+const Item = (props: {
+  text: string;
+  edit?: boolean;
+  setEdit: React.Dispatch<React.SetStateAction<boolean>>;
+  editId: string | undefined;
+  setEditId: React.Dispatch<React.SetStateAction<string | undefined>>;
+  onBlur: () => void;
+}) => {
+  const { text, edit, setEdit, editId, setEditId, onBlur } = props;
   const [finished, setFinished] = useState<boolean>(false);
 
   const strikeThroughStyle: TextStyle = {
     textDecorationLine: "line-through",
     textDecorationStyle: "solid",
+  };
+
+  const checkEditable = () => edit && editId;
+
+  const stopEditing = () => {
+    setEdit(false);
+    setEditId(undefined);
   };
 
   return (
@@ -30,17 +46,32 @@ const Item = (props: { text: string; edit?: boolean; onBlur: () => void }) => {
           </Container>
         </View>
         <View style={{ flex: 1 }}>
-          {edit ? (
-            <TextInput
-              style={{
-                marginHorizontal: 20,
-                marginVertical: 14,
-                fontSize: 18,
-                color: COLORS.white,
-              }}
-              placeholder="Fill you note"
-              onEndEditing={onBlur}
-            />
+          {checkEditable() ? (
+            <>
+              <View style={{ flexDirection: "row" }}>
+                <View style={{ flex: 1 }}>
+                  <TextInput
+                    style={{
+                      marginHorizontal: 20,
+                      marginVertical: 14,
+                      fontSize: 18,
+                      color: COLORS.white,
+                    }}
+                    placeholder="Fill you note"
+                    placeholderTextColor={COLORS.white}
+                    onEndEditing={onBlur}
+                  />
+                </View>
+                <View style={{ alignSelf: "center", flex: 0 }}>
+                  <IconButton
+                    icon="add-circle-outline"
+                    onClick={() => {
+                      return;
+                    }}
+                  />
+                </View>
+              </View>
+            </>
           ) : (
             <TouchableOpacity
               style={{
--- pages/List.tsx
+++ pages/List.tsx
@@ -1,15 +1,16 @@
-import { useEffect, useState } from "react";
+import { SetStateAction, useEffect, useState } from "react";
 import Item from "../components/Item";
 import PageContainer from "../layouts/PageContainer";
 import Button from "../components/Button";
 import { View } from "react-native";
 import IListItem from "../interfaces/ListItem";
 import { getAllCurrentItems } from "../storage/listStorage";
+import TextField from "../components/TextField";
 
 const List = () => {
   const [listItem, setListItem] = useState<Array<IListItem>>();
   const [edit, setEdit] = useState<boolean>(true);
-  
+  const [editId, setEditId] = useState<string | undefined>("askdsa");
 
   useEffect(() => {
     getAllCurrentItems().then((list: Array<IListItem>) => {
@@ -20,11 +21,27 @@ const List = () => {
   return (
     <PageContainer>
       <View style={{ display: "flex", flexDirection: "row-reverse" }}>
-        <Button text="Add" onClick={() => {setEdit(!edit)}} />
+        <Button
+          text="Add"
+          onClick={() => {
+            setEdit(!edit);
+          }}
+        />
       </View>
-      
 
-      <Item text={"Exemplo"}  edit={edit} onBlur={()=>setEdit(false)}/>
+      <Item
+        text={"Exemplo"}
+        edit={edit}
+        setEdit={setEdit}
+        editId={editId}
+        onBlur={() => setEdit(false)}
+        setEditId={setEditId}
+      />
+      {/* <View style={{ display: "flex", flexDirection: "row-reverse" }}>
+        <Button text="Save" onClick={() => {setEdit(!edit)}} />
+      </View> */}
+
+      {listItem?.map((item: IListItem) => <TextField>{item.title}</TextField>)}
     </PageContainer>
   );
 };
